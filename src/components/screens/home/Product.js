0ef6fb4e25a60a117108002f6e613d4ac7438de1
gittyburstein <g0534163997@gmail.com>
2025-04-22 23:34:11 +0300

    basic hierarchical structure

---
+++ src/components/screens/home/Product.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Product() {
+  
+    return (
+     <>
+       <p></p>
+       <img></img>
+       <a></a>
+       <button onClick={iii}>to invite</button>
+       <p></p>
+     </>
+    );
+}
